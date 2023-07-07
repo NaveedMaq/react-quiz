08bbb3ed0e546d0d0da386a8d319bec4a60abe31
NaveedMaq <naveedmaqbool30@gmail.com>
2023-07-07 12:51:05 +0530

    feat: Add startscreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,48 @@
+import { useEffect, useReducer } from 'react';
+import Header from './Header';
+import Loader from './Loader';
+import Error from './Error';
+import Main from './Main';
+import StartScreen from './StartScreen';
+
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      new Error('Unknown action');
+  }
+}
+
 export default function App() {
-  console.log('Hello');
-  return <div>APple</div>;
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((_) => dispatch({ type: 'dataFailed' }));
+  }, []);
+  return (
+    <div className='app'>
+      <Header />
+      <main className='main'>
+        <Main>
+          {status === 'loading' && <Loader />}
+          {status === 'error' && <Error />}
+          {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+        </Main>
+      </main>
+    </div>
+  );
 }
